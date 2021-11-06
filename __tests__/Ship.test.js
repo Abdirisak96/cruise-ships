ebbd7b95482a87efd6d3a2c14ab9fdacfc9d3b4e
Abdirisak Mohamed <abdirisak.mohamed1@hotmail.com>
2021-11-06 10:42:58 +0000

    first commit

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,5 +1,7 @@
 /* globals describe it expect */
 const ship = require('../src/Ship.js');
 describe('ship', () => {
-
+    it('can be instantiated', () => {
+        expect(new ship()).toBeinstanceOff(Object);
+    });   
 });
